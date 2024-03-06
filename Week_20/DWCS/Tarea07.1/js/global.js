c5be1d0ee9853eb39adc3764e5981f717bab07a2
hagarbo <hagarbo@gmail.com>
2024-03-06 20:01:02 +0100

    renaming

---
+++ Week_20/DWCS/Tarea07.1/js/global.js
@@ -0,0 +1,14 @@
+
+const base_url = "http://localhost:3000/controller/FrontController.php";
+var userId = null;
+window.onload = onceLoaded;
+
+
+function onceLoaded() {
+
+    console.debug("window loaded");
+    document.querySelector('#formLogin').onsubmit = login;
+    document.querySelector('#formLogout').onsubmit = confirmLogout;
+    getRoles();
+}
+
